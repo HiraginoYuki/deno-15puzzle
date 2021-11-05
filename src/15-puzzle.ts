580a46bcb818b74139d71dcc4f7bd5aec20bad84
Yuki Hiragino <yuki@shiina.family>
2021-11-05 12:19:20 +0900

    well that was just wrong

--- src/15-puzzle.ts
+++ src/15-puzzle.ts
@@ -29,7 +29,7 @@ export class Pieces extends Array<Readonly<Piece>[]> {
     ) && (
       pieces.every(Number.isInteger) &&
       pieces.every((piece) => 0 <= piece && piece <= pieces.length - 1) &&
-      pieces.length === new Set(pieces).length
+      pieces.length === new Set(pieces).size
     );
   }
   public static check2d(pieces: number[][]): boolean {
