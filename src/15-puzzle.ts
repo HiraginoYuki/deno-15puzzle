9bc33c72789e32d4bece0f9384b797999c3276a5
Yuki Hiragino <yuki@shiina.family>
2021-11-10 12:05:11 +0900

    skypack

--- src/15-puzzle.ts
+++ src/15-puzzle.ts
@@ -1,5 +1,5 @@
 import { Vec2 } from "./vec2.ts";
-import { create } from "https://esm.sh/random-seed";
+import { create } from "https://cdn.skypack.dev/random-seed";
 import { chooseRandom, chooseRandomIndex, range } from "./utils.ts";
 
 export class Piece {
